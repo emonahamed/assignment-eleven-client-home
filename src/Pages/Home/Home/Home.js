66412514aa6eaa9fb394d58d58e5284abc19bc11
emonahamed <emonahamed711@gmail.com>
2022-11-08 14:52:41 +0600

    data fetched

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import PersonalProfile from '../PersonalProfile/PersonalProfile';
 import Ratings from '../Ratings/Ratings';
+import Services from '../Services/Services';
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
         <div>
             <PersonalProfile></PersonalProfile>
             <Banner></Banner>
+            <Services></Services>
             <Ratings></Ratings>
 
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
+    console.log(services);
+
+
+
+
     return (
         <div>
-
+            <p className='text-2xl font-bold'>Services{services.length}</p>
         </div>
     );
 };
